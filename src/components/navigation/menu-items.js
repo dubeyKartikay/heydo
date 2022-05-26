e103e6382774bc69e11cb6a5617333717264b35e
Kartikay Dubey <kartikayd2003@gmail.com>
2022-05-26 16:14:25 +0530

    navbar added

---
+++ src/components/navigation/menu-items.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title:'Home',
+        url:'#',
+        cName : 'nav-links'
+    },
+    {
+        title:'About Us',
+        url:'#',
+        cName : 'nav-links'
+    },
+    {
+        title:'Contact Us',
+        url:'#',
+        cName : 'nav-links'
+    },
+    {
+        title:'Download',
+        url:'#',
+        cName : 'nav-links'
+    },
+    {
+        title:'Sign up',
+        url:'#',
+        cName : 'nav-links-mobile'
+    }
+]
---
+++ src/components/navigation/navigation.component.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { MenuItems } from './menu-items'
+import "./navigation.styles.scss"
+import { Button } from '../button/button.component';
+export default class Navigation extends Component {
+    state = { clicked: false };
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+    render() {
+        return (
+            <nav className='navbar-items'>
+                <h1 className="navbar-logo">Heydo <i className='fab fa-react'></i></h1>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"} ></i>
+                </div>
+                <ul className={this.state.clicked ? 'nav-menu active':"nav-menu"}>
+                    {MenuItems.map((item, index) => {
+                        return (
+
+                            <li key={index} ><a className={item.cName} href={item.url} >
+                                {item.title}
+
+                            </a></li>
+                        )
+                    })}
+                </ul>
+                <Button buttonStyle="btn--outline" >Sign Up</Button>
+            </nav>
+        )
+    }
+}
