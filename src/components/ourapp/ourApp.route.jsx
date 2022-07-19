c05e647b5fd1a1138e37d9b5cf6c6270e56706d7
Kartikay Dubey <kartikayd2003@gmail.com>
2022-07-19 21:27:54 +0530

    changes

--- src/components/ourapp/ourApp.route.jsx
+++ src/components/ourapp/ourApp.route.jsx
@@ -1,19 +1,34 @@
 import React from 'react'
 import video from "../../Assets/heyDoFinalVideo.mp4"
+import ourAppimg from "../../Assets/img2.jpeg"
 import "./ourApp.styles.scss"
 export default function Ourapp() {
     return (
         <div id='our-app'>
             <h1 className='heading' >Our Application</h1>
             <div className="flex-container">
-                <div className="para">
-                    <p sty className='our-app-text'>
-                        Time can easily be called the most precious resource of the contemporary world. With HeydoTech, the task of filling up different forms becomes automated, henceforth saving precious time.
-                        Heydo Tech reduces the manual backend data-entry workload in Automobile Showrooms. It automates the data to be entered in different portals using a bot.
-                        The customer simply has to fill their minimal details online on their device which can be verified by the salesperson and all the data is fed into the portals then and there , reducing all the hassle.
-                    </p>
+                <div>
+                    <div className="para">
+                        <h1>Intro</h1>
+                        <p sty className='our-app-text'>
+                            <li>Time is the most precious resource in today's world.</li>
+                            <br />
+                            <li>With HeydoTech, manual data entry work becomes automated, saving precious time.</li>
+                        </p>
+                    </div>
+                    <video controls> <source src={video} type="video/mp4" /> </video>
                 </div>
-                <video controls> <source src={video} type="video/mp4" /> </video>
+                <div>
+                    <img style={{"maxWidth":"50%",borderRadius:"20px"}} src={ourAppimg} alt="" />
+                    <div className="para">
+                        <h1>Application Flow</h1>
+                        <p sty className='our-app-text'>
+                            <li>Your salesperson just needs to verify the details filled by your customer and Voila! It's done.</li>
+
+                            <li>The bot automatically checks all the data, opens the portals, goes to the specific field and enters everything</li>
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     )
